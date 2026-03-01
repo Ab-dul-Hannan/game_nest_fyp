849fbd45aea435fc9e82054fbd9e7b131ea8b104
Abdul Hannan <abdunknownsamurai@gmail.com>
2026-03-01 15:46:23 +0500

    Update music_service.dart
    when app lifecycle stops music paused

--- lib/music_service.dart
+++ lib/music_service.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/widgets.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-class MusicService {
+class MusicService with WidgetsBindingObserver {
   static final AudioPlayer _player = AudioPlayer();
   static bool _isInitialized = false;
 
@@ -10,6 +11,8 @@ class MusicService {
     await _player.setReleaseMode(ReleaseMode.loop);
     await _player.setVolume(0.5);
     await _player.play(AssetSource('audio/music_gamenest_bg.mp3'));
+
+    WidgetsBinding.instance.addObserver(_AppLifecycleHandler(_player));
 
     _isInitialized = true;
   }
@@ -21,6 +24,21 @@ class MusicService {
   static Future<void> mute(bool isMuted) async {
     if (isMuted) {
       await _player.setVolume(0);
+    }
+  }
+}
+
+class _AppLifecycleHandler extends WidgetsBindingObserver {
+  final AudioPlayer player;
+
+  _AppLifecycleHandler(this.player);
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      player.pause();
+    } else if (state == AppLifecycleState.resumed) {
+      player.resume();
     }
   }
 }
